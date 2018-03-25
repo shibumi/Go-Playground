fb31d92284bbb9ca92ffdef0221ae42744dcdb81
Christian Rebischke <Chris.Rebischke@posteo.de>
2018-03-25 21:33:52 +0200

    implemented a counter and learned how to use functions

--- silly_number_game/silly_number_game.go
+++ silly_number_game/silly_number_game.go
@@ -10,33 +10,46 @@ import (
 	"strings"
 )
 
+func generateRandomNumber() (randNum int64) {
+	return int64(rand.Intn(2001) + 1)
+}
+
 func main() {
-	// Generate random number
-	randNum := int64(rand.Intn(2001) + 1)
+    var endGame bool = false
+    var counter int = 0
+	randNum := generateRandomNumber()
 	fmt.Println("Welcome to my silly number game")
 	fmt.Println("Press 0 to exit the game")
 	reader := bufio.NewReader(os.Stdin)
-	for {
+	for !endGame {
 		fmt.Print("Please enter a number: ")
 		text, err := reader.ReadString('\n')
 		text = strings.Trim(text, "\n")
 		if err != nil {
 			log.Fatal(err)
+			continue
 		}
 		i, err := strconv.ParseInt(text, 10, 32)
 		if err != nil {
 			log.Fatal(err)
+			continue
 		}
-		if i == randNum {
-			fmt.Println("BOOM! You've guessed the right number: ", randNum)
-			break
-		} else if i == 0 {
+		switch {
+		case i == randNum:
+			fmt.Println("Boom! You've guessed the right number: ", randNum)
+            endGame = true
+		case (i > randNum) && (i != 0):
+			fmt.Println("Too high")
+		case (i < randNum) && (i != 0):
+			fmt.Println("Too low")
+		case i == 0:
 			fmt.Println("You have pressed 0. Exiting game...")
-			break
-		} else {
+            endGame = true
+		default:
 			fmt.Println("Too bad. Wrong number.")
 		}
+        counter++
 	}
 	fmt.Println("Thanks for playing!")
-
+    fmt.Println("You've needed ", counter, " tries")
 }
