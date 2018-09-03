90d6cd8f0f14a958a45ff0aba475b318c135ffe4
Christian Rebischke <chris@nullday.de>
2018-09-03 04:12:54 +0200

    added sqrt.go

---
+++ sqrt/sqrt.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// This is the example code out of the go-tour
+// that you can find in flowcontrol/8
+func Sqrt(x float64, d float64) (y float64) {
+	z := 1.0
+	y = z * 2
+	for math.Abs(z-y) > d {
+		y = z
+		z = z - ((math.Pow(z, 2) - x) / 2 * z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2, 0.001))
+	fmt.Println(math.Sqrt(2))
+}
