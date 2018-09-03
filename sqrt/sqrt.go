a7472ae755141084f4513c950dc1aa59866790c5
Christian Rebischke <chris@nullday.de>
2018-09-03 04:16:06 +0200

    fixed return value
    Signed-off-by: Christian Rebischke <chris@nullday.de>

--- sqrt/sqrt.go
+++ sqrt/sqrt.go
@@ -7,9 +7,9 @@ import (
 
 // This is the example code out of the go-tour
 // that you can find in flowcontrol/8
-func Sqrt(x float64, d float64) (y float64) {
-	z := 1.0
-	y = z * 2
+func Sqrt(x float64, d float64) (z float64) {
+	z = 1.0
+	y := z * 2
 	for math.Abs(z-y) > d {
 		y = z
 		z = z - ((math.Pow(z, 2) - x) / 2 * z)
