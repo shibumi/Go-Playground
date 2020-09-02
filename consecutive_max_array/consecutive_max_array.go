3b0813861a27429c816d61ce78cae90e5dbdd26f
Christian Rebischke <chris@shibumi.dev>
2020-09-03 00:34:09 +0200

    add first draft for google kickstart
    This doesn't work yet..

--- consecutive_max_array/consecutive_max_array.go
+++ consecutive_max_array/consecutive_max_array.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func calcDifference(x uint, y uint) uint {
 	if x >= y {
 		return x - y
@@ -31,4 +40,32 @@ func algorithmicArray(input []uint) (solution []uint) {
 		}
 	}
 	return hashMap[key]
+}
+
+func main() {
+	//algorithmicArray([]uint{10, 7, 4, 6, 8, 10, 11})
+	scanner := bufio.NewScanner(os.Stdin)
+	// just scan first line to get number of tests
+	scanner.Scan()
+	i := 1
+	caseNr := 1
+	for scanner.Scan() {
+		if i%2 != 0 {
+			i++
+			continue
+		}
+		line := strings.Split(scanner.Text(), " ")
+		array := []uint{}
+		for _, v := range line {
+			n, err := strconv.ParseUint(v, 10, 64)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			i := uint(n)
+			array = append(array, i)
+		}
+		fmt.Printf("Case #%d: %d\n", caseNr, len(algorithmicArray(array)))
+		caseNr++
+		i++
+	}
 }
